b2347f0593232388103d767d2162147761163888
Matteo Hertel <info@matteohertel.com>
2018-12-01 19:16:47 +0000

    [Day1] add second star

--- 2018/day1/main.go
+++ 2018/day1/main.go
@@ -9,18 +9,36 @@ import (
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	var frequencies []int
-
-	for scanner.Scan() {
-		i, _ := strconv.Atoi(scanner.Text())
-		frequencies = append(frequencies, i)
-	}
-
-	if err := scanner.Err(); err != nil {
+	frequencies, err := getInput()
+	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Final sum", SumFrequencies(&frequencies))
+	fmt.Println("Final sum: ", SumFrequencies(&frequencies))
+	fmt.Println("First cycle:", DetectCyleFrequency(&frequencies, 136))
+}
+
+func DetectCyleFrequency(frequencies *[]int, repeatCycle int) int {
+
+	sum := 0
+	memo := map[string]bool{strconv.Itoa(sum): true}
+
+	for i := 0; i < repeatCycle; i++ {
+
+		for _, frequency := range *frequencies {
+			newFrequency := sum + frequency
+			sum = newFrequency
+			key := strconv.Itoa(newFrequency)
+
+			if _, ok := memo[key]; ok {
+				return newFrequency
+			} else {
+				memo[key] = true
+			}
+
+		}
+	}
+	fmt.Println("No Cycle Detected")
+	return 0
 }
 
 func SumFrequencies(frequencies *[]int) int {
@@ -39,4 +57,21 @@ func debugFrequencies(frequencies *[]int) {
 		fmt.Printf("Addr: %p\n", &frequency)
 		fmt.Println("")
 	}
+}
+
+func getInput() ([]int, error) {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	var frequencies []int
+
+	for scanner.Scan() {
+		i, _ := strconv.Atoi(scanner.Text())
+		frequencies = append(frequencies, i)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return frequencies, nil
+
 }
--- 2018/day1/main_test.go
+++ 2018/day1/main_test.go
@@ -4,6 +4,14 @@ import (
 	"testing"
 )
 
+func TestClycleOfSlice(t *testing.T) {
+	input := []int{1, 1, 3, 4, -1, -10, 5, 1, 4, -2}
+	cycle := DetectCyleFrequency(&input, 10)
+	if cycle != 8 {
+		t.Error("Expect cycled frequency to be 8 but got", cycle)
+	}
+}
+
 func TestSumOfslice(t *testing.T) {
 	input := []int{1, -1, 3, 50, -11}
 	sum := SumFrequencies(&input)
