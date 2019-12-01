6bda2c5dd2fc05c133535b26a557b48669197141
Matteo Hertel <info@matteohertel.com>
2019-12-01 22:38:25 +0000

    day1: add both solutions and tests

--- 2019/day1/__tests__/index.ts
+++ 2019/day1/__tests__/index.ts
@@ -1,6 +1,16 @@
-describe("Test", () => {
-  it("Should Pass", () => {
-    expect("true").toBe("true");
+import { fuelRequirement, recursiveFuelRequirement } from "../";
+
+describe("Fuel requirement", () => {
+  it("correctly calculate the fuel requirement", () => {
+    expect(fuelRequirement(12)).toBe(2);
+    expect(fuelRequirement(14)).toBe(2);
+    expect(fuelRequirement(1969)).toBe(654);
+    expect(fuelRequirement(100756)).toBe(33583);
+  });
+  it("correctly calculate the recursive fuel requirement", () => {
+    expect(recursiveFuelRequirement(14)).toBe(2);
+    expect(recursiveFuelRequirement(1969)).toBe(966);
+    expect(recursiveFuelRequirement(100756)).toBe(50346);
   });
 });
 
--- 2019/day1/index.ts
+++ 2019/day1/index.ts
@@ -1,11 +1,22 @@
-interface Person {
-  name: string;
-  surname: string;
+import * as rawInput from "./input.json";
+const { input } = rawInput;
+
+const solution1 = input.reduce(calculateFuelRequirement, 0);
+const solution2 = input.reduce(calculateRecursiveFuelRequirement, 0);
+console.log({ solution1, solution2 });
+
+function calculateFuelRequirement(acc: number, input: number): number {
+  return acc + fuelRequirement(input);
+}
+function calculateRecursiveFuelRequirement(acc: number, input: number): number {
+  return acc + recursiveFuelRequirement(input);
 }
 
-const p1: Person = {
-  name: "Alan",
-  surname: "Rickman"
-};
-
-console.log({ p1 });
+export function recursiveFuelRequirement(module: number): number {
+  const fuelRequired = fuelRequirement(module);
+  if (fuelRequired < 0) return 0;
+  return fuelRequired + recursiveFuelRequirement(fuelRequired);
+}
+export function fuelRequirement(mass: number): number {
+  return Math.floor(mass / 3) - 2;
+}
